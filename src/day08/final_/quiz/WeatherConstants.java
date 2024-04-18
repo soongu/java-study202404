a54e3d444f16a3ca8fe8def40f8c5921616c2826
soongu <naong526@naver.com>
2024-04-18 15:55:51 +0900

    # final, 상수

---
+++ src/day08/final_/quiz/WeatherConstants.java
@@ -0,0 +1,10 @@
+package day08.final_.quiz;
+
+public class WeatherConstants {
+
+    public static final double MAX_TEMPERATURE_ALERT = 40.0;
+    public static final double MIN_TEMPERATURE_ALERT = -10.0;
+    public static final double PRECIPITATION_ALERT = 50.0;
+
+    private WeatherConstants() {} // 객체 생성 방지
+}
